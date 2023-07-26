86ce34c84f5581d91bda7e105377a89803cced2f
AFK <114419130+cluefix@users.noreply.github.com>
2023-07-26 12:18:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from numba import jit
 import threading
 import time
 import os
-import shutil
+from PIL import Image
 
 colorama.init()
 
@@ -20,19 +20,19 @@ def get_ascii_char(gray_val):
     index = int(gray_val / 255 * (len(ASCII_CHARS) - 1))
     return ASCII_CHARS[index]
 
-def display_ascii_art(frame):
+def display_ascii_art(frame, fps):
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     height, width = gray_frame.shape
-    term_size = shutil.get_terminal_size()
+    term_size = os.get_terminal_size()
     new_width = term_size.columns
     new_height = int(new_width * height / (width * 2))
-    if new_height > term_size.lines:
-        new_height = term_size.lines
+    if new_height > term_size.lines - 1:
+        new_height = term_size.lines - 1
         new_width = int(new_height * width * 2 / height)
     resized_frame = cv2.resize(gray_frame, (new_width, new_height))
 
-    colored_ascii_art = np.empty_like(resized_frame, dtype=object)
+    ascii_colors = np.empty_like(resized_frame, dtype=object)
     for y in range(new_height):
         for x in range(new_width):
             gray_val = resized_frame[y, x]
@@ -45,11 +45,14 @@ def display_ascii_art(frame):
                 r, g, b = rgb_color
                 ascii_color = f"\033[38;2;{r};{g};{b}m"
 
-            colored_ascii_art[y, x] = f"{ascii_color}{char}{Style.RESET_ALL}"
+            ascii_colors[y, x] = f"{ascii_color}{char}{Style.RESET_ALL}"
 
-    ascii_art = '\n'.join([''.join(row) for row in colored_ascii_art])
-    os.system('cls' if os.name == 'nt' else 'clear')  # Use 'cls' on Windows and 'clear' on Linux/macOS
-    print(ascii_art, end='\r')
+    ascii_art = '\n'.join([''.join(row) for row in ascii_colors])
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(ascii_art)
+
+    print(f"FPS: {fps:.2f}", end='\r')
+
 
 def process_frames(cap):
     prev_time = time.time()
@@ -64,7 +67,7 @@ def process_frames(cap):
         time_diff = curr_time - prev_time
         fps = 1.0 / time_diff if time_diff > 0 else 0
         prev_time = curr_time
-        display_ascii_art(frame)
+        display_ascii_art(frame, fps)
 
         cv2.waitKey(1)
 
@@ -75,9 +78,12 @@ def main():
     if not cap.isOpened():
         print("Error opening video file.")
         return
-
+    print('Opening Video...')
     frame_thread = threading.Thread(target=process_frames, args=(cap,))
+    print('Starting frame thread...')
     frame_thread.start()
+    print('Finished frame thread...')
+    print('loading video...')
 
     frame_thread.join()
 
